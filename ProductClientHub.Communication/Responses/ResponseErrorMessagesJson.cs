a1182f6328edeaeec37b5a733a594a50d6cd8bdb
NeiJunio <neijunio50@gmail.com>
2025-02-03 01:12:41 -0300

    Create repository

---
+++ ProductClientHub.Communication/Responses/ResponseErrorMessagesJson.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.NetworkInformation;
+
+namespace ProductClientHub.Communication.Responses
+{
+    public class ResponseErrorMessagesJson
+    {
+        public List<string> Errors { get; private set; }
+
+        public ResponseErrorMessagesJson(string message)
+        {
+            Errors = [message];
+        }
+    }
+}
